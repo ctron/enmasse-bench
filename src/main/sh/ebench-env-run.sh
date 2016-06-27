99d153ecee85f9062349f5f3d5b72cda0d28396c
Ulf Lilleengen <lulf@redhat.com>
2016-06-27 09:18:08 +0200

    Loop forever

--- src/main/sh/ebench-env-run.sh
+++ src/main/sh/ebench-env-run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # Launches ebench from environment variables
- ARGS="-c $BENCH_CLIENTS -h $MESSAGING_SERVICE_HOST -p $MESSAGING_SERVICE_PORT -a $BENCH_ADDRESS -d $BENCH_DURATION -s $BENCH_MSG_SIZE"
- echo "Launching with args: $ARGS"
-./ebench/bin/ebench $ARGS
+ARGS="-c $BENCH_CLIENTS -h $MESSAGING_SERVICE_HOST -p $MESSAGING_SERVICE_PORT -a $BENCH_ADDRESS -d $BENCH_DURATION -s $BENCH_MSG_SIZE"
+echo "Launching with args: $ARGS"
+while true
+do
+    ./ebench/bin/ebench $ARGS
+    sleep 2
+done
