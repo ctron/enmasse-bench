d8c57c3536a1c5c436a2942cfdfa0410fdfa3f3c
Ulf Lilleengen <lulf@redhat.com>
2016-06-27 08:57:22 +0200

    Print arguments

--- src/main/sh/ebench-env-run.sh
+++ src/main/sh/ebench-env-run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Launches ebench from environment variables
-
-./ebench/bin/ebench -c $BENCH_CLIENTS -h $MESSAGING_SERVICE_HOST -p $MESSAGING_SERVICE_PORT -a $BENCH_ADDRESS -d $BENCH_DURATION -s $BENCH_MSG_SIZE
+ ARGS="-c $BENCH_CLIENTS -h $MESSAGING_SERVICE_HOST -p $MESSAGING_SERVICE_PORT -a $BENCH_ADDRESS -d $BENCH_DURATION -s $BENCH_MSG_SIZE"
+ echo "Launching with args: $ARGS"
+./ebench/bin/ebench $ARGS
