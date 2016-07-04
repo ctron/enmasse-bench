49b89d875a038bb4aebf9db14dd98cf6a4786927
Ulf Lilleengen <lulf@redhat.com>
2016-07-04 08:59:53 +0200

    Support wait time when running in openshift

--- ebench-agent/src/main/sh/ebench-agent-run.sh
+++ ebench-agent/src/main/sh/ebench-agent-run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # Launches ebench from environment variables
 ARGS="-c $BENCH_CLIENTS -h $MESSAGING_SERVICE_HOST -p $MESSAGING_SERVICE_PORT -a $BENCH_ADDRESS -d $BENCH_DURATION -s $BENCH_MSG_SIZE -m collector"
+if [ "$BENCH_WAIT_TIME" != "" ]
+then
+    ARGS="$ARGS -w $BENCH_WAIT_TIME"
+fi
 echo "Launching with args: $ARGS"
 while true
 do
