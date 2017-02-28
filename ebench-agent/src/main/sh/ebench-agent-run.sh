28e4961d2a844198f5a2ea88bcbe056fa330cc80
Ulf Lilleengen <lulf@redhat.com>
2017-02-28 11:32:12 +0100

    Do argument setup in template

--- ebench-agent/src/main/sh/ebench-agent-run.sh
+++ ebench-agent/src/main/sh/ebench-agent-run.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
 # Launches ebench from environment variables
-ARGS="-s $BENCH_SENDERS -r $BENCH_RECEIVERS -h $MESSAGING_SERVICE_HOST:$MESSAGING_SERVICE_PORT -a $BENCH_ADDRESS -d $BENCH_DURATION -m $BENCH_MSG_SIZE -f none"
-if [ "$BENCH_WAIT_TIME" != "" ]
-then
-    ARGS="$ARGS -w $BENCH_WAIT_TIME"
-fi
-
-if [ "$BENCH_SPLIT_CLIENTS" != "" ]
-then
-    ARGS="$ARGS -c"
-fi
-
-echo "Launching with args: $ARGS"
-./ebench-agent/bin/ebench-agent $ARGS
+echo "Launching with args: $BENCH_ARGS"
+./ebench-agent/bin/ebench-agent $BENCH_ARGS
