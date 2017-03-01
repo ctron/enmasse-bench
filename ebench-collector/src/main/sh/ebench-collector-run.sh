a21d0a836dfe5dfd101066c5c513b500d399e04f
Ulf Lilleengen <lulf@redhat.com>
2017-03-01 13:11:46 +0100

    Fetch all arguments from environment

--- ebench-collector/src/main/sh/ebench-collector-run.sh
+++ ebench-collector/src/main/sh/ebench-collector-run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # Launches ebench from environment variables
-ARGS="-i $BENCH_INTERVAL"
-echo "Launching with args: $ARGS"
-./ebench-collector/bin/ebench-collector $ARGS
+echo "Launching with args: $BENCH_ARGS"
+./ebench-collector/bin/ebench-collector $BENCH_ARGS
